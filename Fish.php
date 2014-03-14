84eb5fe9cdd713b868a5908902c60a58e4f6f2bf
Samuel Coleman <samuel@seenet.ca>
2014-03-14 16:44:28 -0300

    Build PHP object definitions for fish and sessions.

---
+++ Fish.php
@@ -0,0 +1,88 @@
+<?php namespace ca\acadiau\cs\comp4583\fishtail\data;
+
+/**
+ * A caught fish.
+ *
+ * @since 1.0.0
+ * @author Samuel Coleman <105709c@acadiau.ca>
+ */
+class Fish
+{
+    /**
+     * @var string the species of fish
+     */
+    public $species;
+    /**
+     * @var double the length of the fish in millimetres
+     */
+    public $length;
+    /**
+     * @var bool whether the length of the fish is exact
+     */
+    public $exactLength;
+    /**
+     * @var string the health of the fish when caught
+     */
+    public $catchHealth;
+    /**
+     * @var string the health of the fish when released
+     */
+    public $releaseHealth;
+    /**
+     * @var whether or not the fish is tagged
+     */
+    public $tagged;
+    /**
+     * @var string the fish tag ID
+     */
+    public $tagId;
+    /**
+     * @var string the colour of the fish tag
+     */
+    public $tagColor;
+    /**
+     * @var bool whether or not a scale sample was taken
+     */
+    public $tookSample;
+    /**
+     * @var string a photo of the fish
+     */
+    public $photo;
+
+    public function __construct($object)
+    {
+        if (!isset($object->species))
+            throw new \Exception('A species must be specified');
+        if (!isset($object->length))
+            throw new \Exception('A length must be specified');
+        if (!isset($object->exactLength))
+            throw new \Exception('Whether the length is exact must be specified');
+        if (!isset($object->catchHealth))
+            throw new \Exception('The catch health must be specified');
+        if ($object->catchHealth !== 'dead' && !isset($object->releaseHealth))
+            throw new \Exception('The release health must be specified');
+
+        $this->species = $object->species;
+        $this->length = $object->length;
+        $this->exactLength = $object->exactLength;
+        $this->catchHealth = $object->catchHealth;
+        $this->releaseHealth = ($this->catchHealth !== 'dead') ? $object->releaseHealth : 'dead';
+
+        if ((isset($object->tagged) && $object->tagged)
+            || (isset($object->tagId) && $object->tagId != null
+                && isset($object->tagColor) && $object->tagColor != null))
+        {
+            $this->tagged = true;
+            $this->tagId = (isset($object->tagId)) ? $object->tagId : null;
+            $this->tagColor = (isset($object->tagColor)) ? $object->tagColor : null;
+            $this->tookSample = false;
+        }
+        else
+        {
+            $this->tagged = false;
+            $this->tagId = null;
+            $this->tagColor = null;
+            $this->tookSample = (isset($object->tookSample) && $object->tookSample);
+        }
+    }
+}
---
+++ FishingSession.php
@@ -0,0 +1,95 @@
+<?php namespace ca\acadiau\cs\comp4583\fishtail\data;
+
+/**
+ * A fishing session.
+ *
+ * @since 1.0.0
+ * @author Samuel Coleman <105709c@acadiau.ca>
+ */
+class FishingSession
+{
+    /**
+     * @var double general latitude of session
+     */
+    public $latitude;
+    /**
+     * @var double general longitude of session
+     */
+    public $longitude;
+    /**
+     * @var string the name of the location per database enum
+     */
+    public $locationName;
+    /**
+     * @var \DateTime session start time
+     */
+    public $startDate;
+    /**
+     * @var \DateTime session end time
+     */
+    public $endDate;
+    /**
+     * @var int the number of other anglers in the area
+     */
+    public $anglers;
+    /**
+     * @var bool whether the number of anglers is exact
+     */
+    public $exactAnglers;
+    /**
+     * @var int the number of fishing lines involved in the session
+     */
+    public $lines;
+    /**
+     * @var int the total number of fish caught throughout the session
+     */
+    public $catches;
+    /**
+     * @var bool whether the number of fish caught is exact
+     */
+    public $exactCatches;
+
+    /**
+     * @var \ca\acadiau\cs\comp4583\fishtail\data\Fish[] the fish in the session
+     */
+    public $fish;
+
+    public function __construct($object)
+    {
+        if (!isset($object->startDate))
+            throw new \Exception('A start date must be specified');
+        if (!isset($object->endDate))
+            throw new \Exception('An end date must be specified');
+        if (!isset($object->anglers))
+            throw new \Exception('The number of anglers must be specified');
+        if (!isset($object->exactAnglers))
+            throw new \Exception('Whether the number of anglers is exact must be specified');
+        if (!isset($object->lines))
+            throw new \Exception('The number of lines must be specified');
+        if (!isset($object->catches))
+            throw new \Exception('The number of catches must be specified');
+        if (!isset($object->exactCatches))
+            throw new \Exception('Whether the number of catches is exact must be specified');
+        if (!isset($object->fish) || !is_array($object->fish))
+            throw new \Exception('An array of fish must be given');
+
+        if ((!isset($object->latitude) || !isset($object->longitude))
+            && !isset($object->locationName))
+            throw new \Exception('A location must be specified');
+
+        $this->latitude = (isset($object->latitude)) ? $object->latitude : null;
+        $this->longitude = (isset($object->longitude)) ? $object->longitude : null;
+        $this->locationName = (isset($object->locationName)) ? $object->locationName : null;
+        $this->startDate = $object->startDate;
+        $this->endDate = $object->endDate;
+        $this->anglers = $object->anglers;
+        $this->exactAnglers = $object->exactAnglers;
+        $this->lines = $object->lines;
+        $this->catches = $object->catches;
+        $this->exactCatches = $object->exactCatches;
+
+        $this->fish = [];
+        foreach ($object->fish as $fish)
+            $this->fish[] = new Fish($fish);
+    }
+}
